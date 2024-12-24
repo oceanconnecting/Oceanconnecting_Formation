3b6c1830156005bbc140e69c5f5fef46ce7a7698
Ocean_Dev2 <zakaryabaouali255@gmail.com>
2024-12-24 13:00:02 +0100

    cvc

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -8,6 +8,8 @@ import Header1 from "./header/Header1"
 import Header2 from './header/Header2'
 import Header3 from "./header/Header3"
 import Script from "next/script";
+
+import { GoogleAnalytics } from '@next/third-parties/google'
 export default function Layout({ headerStyle, footerStyle, headTitle, breadcrumbTitle, children }) {
     const [scroll, setScroll] = useState(0)
     // Moblile Menu
@@ -29,24 +31,26 @@ export default function Layout({ headerStyle, footerStyle, headTitle, breadcrumb
 
     return (
         <>
+            <GoogleAnalytics gaId="G-LHCVR17P5K" />
             <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=G-LHCVR17P5K`}
                 strategy="afterInteractive"
-                src="https://www.googletagmanager.com/gtag/js?id=G-LHCVR17P5K"
             />
             <Script
                 id="google-analytics"
                 strategy="afterInteractive"
                 dangerouslySetInnerHTML={{
                     __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-LHCVR17P5K', {
-                page_path: window.location.pathname,
-              });
-            `,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LHCVR17P5K', {
+              page_path: window.location.pathname,
+            });
+          `,
                 }}
             />
+
             <PageHead headTitle={headTitle} />
 
             {!headerStyle && <Header1 scroll={scroll} isMobileMenu={isMobileMenu} handleMobileMenu={handleMobileMenu} />}
